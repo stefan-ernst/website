d3a4813e613a8080f02239167ea755cd188d9277
Stefan Ernst <ernst.stefan@icloud.com>
2020-08-14 19:35:58 +0200

    Saving Next site

---
+++ pages/privacy.js
@@ -0,0 +1,22 @@
+import Header from "../components/header";
+import Nav from "../components/nav";
+import Footer from "../components/footer";
+
+export default function Blog() {
+
+    return (
+        <>
+            <Header />
+            <div className='grid-container '>
+                <Nav />
+
+                <main className='Main'>
+
+
+                </main>
+
+                <Footer />
+            </div>
+        </>
+    );
+}
